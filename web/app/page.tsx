2becb104bad673709934927c06f1870402543cee
Sudip Biswas <sudipbiswas2142@gmail.com>
2026-02-05 10:26:13 +0530

    feat: add Dockerfile for API and update homepage redirection

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import api from "@/lib/api";
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <main className="flex flex-col items-center justify-center gap-y-6 min-h-dvh">
-      <h1 className="text-4xl font-bold">Task Manager</h1>
-      <Button
-        onClick={async () => {
-          await api.get("/").then((res) => alert(JSON.stringify(res.data)));
-        }}
-      >
-        Make request
-      </Button>
-    </main>
-  );
+  return redirect("/dashboard");
 }
